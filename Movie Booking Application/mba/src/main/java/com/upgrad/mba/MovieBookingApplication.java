526086fd2638e713f5aeb9d67342da252b4e411a
faisal <faisal25march99@gmail.com>
2020-12-06 20:00:11 +0530

    Done with Session-2  1. application.properties 2. pom.xml 3. @SpringBootApplication 4. Set Active Profile 5. Spring Boot lazy initialization

--- Movie Booking Application/mba/src/main/java/com/upgrad/mba/MovieBookingApplication.java
+++ Movie Booking Application/mba/src/main/java/com/upgrad/mba/MovieBookingApplication.java
@@ -1,13 +1,19 @@
 package com.upgrad.mba;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+import javax.sql.DataSource;
+
+@SpringBootApplication // Responsible for all the auto configuration
 public class MovieBookingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieBookingApplication.class, args);
+
+		ConfigurableApplicationContext _context =  SpringApplication.run(MovieBookingApplication.class, args);
+		DataSource soruce = _context.getBean(DataSource.class);
 	}
 
 }
