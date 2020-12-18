fbf7d7cf474f7e1e95776cf4654f4edcc7d38c9f
faisal <faisal25march99@gmail.com>
2020-12-18 10:31:30 +0530

    Steps to run application in war mode 1. pom.xml change jar --> war 2. Create ServletInitializer.java and extends SpingBootServletInitializer

--- Movie Booking Application/mba/src/main/java/com/upgrad/mba/ServletInitializer.java
+++ Movie Booking Application/mba/src/main/java/com/upgrad/mba/ServletInitializer.java
@@ -7,5 +7,5 @@ public class ServletInitializer extends SpringBootServletInitializer {
 @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
     return application.sources(MovieBookingApplication.class) ;
-}
+ }
 }
