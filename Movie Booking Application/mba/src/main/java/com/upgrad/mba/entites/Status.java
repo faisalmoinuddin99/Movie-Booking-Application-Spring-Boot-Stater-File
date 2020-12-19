249ba2e48c3e3315b8c79bf2e1475b4d457371df
faisal <faisal25march99@gmail.com>
2020-12-19 15:55:35 +0530

    Done with HomeWork, Created Entity Classes for UserType, Status, Theatre, City, Language, Booking

--- Movie Booking Application/mba/src/main/java/com/upgrad/mba/entites/Status.java
+++ Movie Booking Application/mba/src/main/java/com/upgrad/mba/entites/Status.java
@@ -1,14 +1,41 @@
 package com.upgrad.mba.entites;
 
-import javax.persistence.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Status {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int status_id ;
+    @GeneratedValue
+    private int statusId;
 
-    @Column(nullable = false, unique = true)
-    private String status_name ;
+    @Column(length = 20, nullable = false, unique = true)
+    private String statusName;
+
+    public int getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(int statusId) {
+        this.statusId = statusId;
+    }
+
+    public String getStatusName() {
+        return statusName;
+    }
+
+    public void setStatusName(String statusName) {
+        this.statusName = statusName;
+    }
+
+    @Override
+    public String toString() {
+        return "Status{" +
+                "statusId=" + statusId +
+                ", statusName='" + statusName + '\'' +
+                '}';
+    }
 }
--- Movie Booking Application/mba/src/main/java/com/upgrad/mba/entites/UserType.java
+++ Movie Booking Application/mba/src/main/java/com/upgrad/mba/entites/UserType.java
@@ -1,14 +1,41 @@
 package com.upgrad.mba.entites;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
-@Table(name = "UserType")
 public class UserType {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int userTypeId ;
 
-    @Column(unique = true,columnDefinition = "varchar(255) default 'User'" )
-    private  String userTypeName ;
+    @Id
+    @GeneratedValue
+    private int userTypeId;
+
+    @Column(length = 20, unique = true)
+    private String userTypeName = "User";
+
+    public int getUserTypeId() {
+        return userTypeId;
+    }
+
+    public void setUserTypeId(int userTypeId) {
+        this.userTypeId = userTypeId;
+    }
+
+    public String getUserTypeName() {
+        return userTypeName;
+    }
+
+    public void setUserTypeName(String userTypeName) {
+        this.userTypeName = userTypeName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserType{" +
+                "userTypeId=" + userTypeId +
+                ", userTypeName='" + userTypeName + '\'' +
+                '}';
+    }
 }
